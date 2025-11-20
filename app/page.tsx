463c3bf37be3f4d478ca906958e4f5036030b000
John Michael <byjohnmichael@gmail.com>
2025-11-19 20:12:30 -0700

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import Desktop from "@/src/components/Desktop/Desktop";
-import ErrorBoundary from "@/src/components/ErrorBoundary/ErrorBoundary";
-import ErrorLogger from "@/src/components/ErrorLogger/ErrorLogger";
 
 export default function Home() {
     return (
-        <ErrorBoundary>
-            <div className="App">
-                <Desktop />
-                <SpeedInsights />
-                <ErrorLogger />
-            </div>
-        </ErrorBoundary>
+        <div className="App">
+            <Desktop />
+            <SpeedInsights />
+        </div>
     );
 } // By John Michael
